d80ae959434ee9f50db7f559c9197c613309e9f3
Rafael Melo <rafaelhsmelo@hotmail.com>
2024-07-29 23:04:17 -0300

    .

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,9 +1,19 @@
 function adicionar(params) {
-    let itemTexto = document.getElementById('produto').value; //Encontra texto que descreve item e preço
-    let preco = parseInt(itemTexto.substring((itemTexto.search('-')+4))); //Separa o valor do preço
-    alert(preco);
+    let item = document.getElementById('produto').value; //Encontra texto que descreve item e preço
+    let preco = parseInt(item.substring((item.search('-') + 4))); //Separa o valor do item
+    let nomeItem = item.substring(0, (item.search('-') - 1)); //Separa o nome do item
+    let quantidade = document.getElementById('quantidade').value; //Encontra a quantidade 
+    let carrinho = [];
+    let total = document.getElementById('valor-total').innerText;
+
+    alert(total+'  '+carrinho);
+
+    // if (quantidade < 1 || isNaN(quantidade)) { //Checa se quantidade é valida
+    //     return;
+    // } else {
+    // }
 }
 
-function limpar(){
+function limpar() {
 
 }
