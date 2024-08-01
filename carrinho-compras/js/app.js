1916fd1cca19cdbe67dbf791f3854cb4383f943d
Rafael Melo <rafaelhsmelo@hotmail.com>
2024-07-31 21:10:58 -0300

    Adicionado verificação de quantidade válida

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -3,23 +3,27 @@ document.getElementById('lista-produtos').innerHTML = '';
 document.getElementById('calor-total').textContent = `R$ 0`;
 
 function adicionar(params) {
-    
-    let item = document.getElementById('produto').value; 
+
+    let item = document.getElementById('produto').value;
     let itemNome = item.split('-')[0];
     let itemValor = item.split('R$')[1];
-    let quantidade = document.getElementById('quantidade').value; 
+    let quantidade = document.getElementById('quantidade').value;
     let subTotal = quantidade * itemValor;
 
-    let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML += `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}</span> ${itemNome} <span class="texto-azul">${subTotal}</span>
-        </section>`;
+    if (quantidade < 1 || isNaN(quantidade)) {
+        return;
+    } else {
+        let carrinho = document.getElementById('lista-produtos');
+        carrinho.innerHTML += `<section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}</span> ${itemNome} <span class="texto-azul">${subTotal}</span>
+            </section>`;
 
-    totalCarrinho += subTotal;
-    let totalExibido = document.getElementById('valor-total');
-    totalExibido.textContent = `R$ ${totalCarrinho}`;
+        totalCarrinho += subTotal;
+        let totalExibido = document.getElementById('valor-total');
+        totalExibido.textContent = `R$ ${totalCarrinho}`;
 
-    document.getElementById('quantidade').value = 0;
+        document.getElementById('quantidade').value = 0;
+    }
 
     // if (quantidade < 1 || isNaN(quantidade)) { //Checa se quantidade é valida
     //     return;
