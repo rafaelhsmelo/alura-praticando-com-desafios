9b8aa8abf7c9d413bda660d5811b592606b9f728
rafaelhsmelo <rafaelhsmelo@hotmail.com>
2024-08-12 10:19:39 -0300

    Versão finalizada

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,38 +1,42 @@
-//Receber tipo de ingresso
-//Receber quantidade
-//Checar disponibilidade 
-let qntPista = document.getElementById('qtd-pista');
-let qntSuperior = document.getElementById('qtd-superior');
-let qntInferior = document.getElementById('qtd-inferior');
-
 function comprar() {
     let qntDeIngresso = parseInt(document.getElementById('qtd').value);
     let tipoDeIngresso = document.getElementById('tipo-ingresso').value;
 
+    //Valida a quantidade digitada
     if (qntDeIngresso < 1 || isNaN(qntDeIngresso)) {
         alert('Quantidade inválida');
     } else {
+
+        //Recebe o tipo e chama a função de checarCompra()
         switch (tipoDeIngresso) {
             case 'inferior':
-                qntInferior.innerText -= qntDeIngresso;
+                checarCompra(qntDeIngresso,'qtd-inferior');
                 break;
 
             case 'superior':
-                qntSuperior.innerText -= qntDeIngresso;
+                checarCompra(qntDeIngresso,'qtd-superior');
                 break;
 
             case 'pista':
-                qntPista.innerText -= qntDeIngresso;
+                checarCompra(qntDeIngresso,'qtd-pista');
                 break;
 
             default:
                 alert('Ocorreu um erro, tente novamente.')
                 break;
         }
-
-
     }
+}
 
+//Subrai o valor comprado do total e atualiza o texto
+function checarCompra(qntDeIngresso,id) {
+    let tipoDeIngresso = parseInt(document.getElementById(id).textContent);
 
-
+    if (qntDeIngresso > tipoDeIngresso) {
+        alert('Não há ingressos suficientes. Tente novamente');
+    } else {
+        tipoDeIngresso -= qntDeIngresso;
+        document.getElementById(id).textContent = tipoDeIngresso;
+        alert ('Compra efetuada com sucesso');
+    }
 }
